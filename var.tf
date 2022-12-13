583dec07633480368a0784453b233d5949b22da2
vsaicharanreddy <119204401+vsaicharanreddy@users.noreply.github.com>
2022-12-13 12:58:42 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-westt-2" 
+       default     = "us-west-2" 
 }
 
 variable "key_name" { 
